f67fbb52afbda9608eaf34e7201fdfc5fa86a678
Synergized <synergized@iMac-de-Synergized.local>
2017-06-02 17:43:27 +0200

    working prompts for form generator

--- generators/form/index.js
+++ generators/form/index.js
@@ -1,17 +1,47 @@
-var Generator = require('yeoman-generator')
+const Generator = require('yeoman-generator')
+
+const questions = [
+    { type: 'input',
+      name: 'name',
+      message: 'What is the name of this form?',
+      default: 'someForm'
+    }
+  ]
+
+const names = [
+    {
+      type: 'input',
+      name: 'inputs',
+      message: 'What is the name of the input?',
+      default: '.input'
+    },
+    {
+      type: 'confirm',
+      name: 'another',
+      message: "Is there another input?",
+      default: true
+    }
+  ]
+
+  const inputs = []
 
 module.exports = class extends Generator {
 
-   method1() {
-      this.log('form method 1 just ran')
+   prompting() {
+     var self = this
+      return this.prompt(questions).then(function (answers) {
+        self.formName = answers.name
+        console.log(`self.formName`, self.formName)
+    });
    }
 
-   method2() {
-      this.log('form method 2 just ran')
-   }
-
-   _private_method() {
-      console.log('private hey');
-   }
+  prompting2 () {
+    const self = this
+    return this.prompt(names).then(function (answers) {
+      inputs.push(answers.inputs)
+      if (answers.another) self._inputs()
+      else self.inputs = inputs
+    })
+  }
 
 }
