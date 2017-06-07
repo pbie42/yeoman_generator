33b89826d7b54f9a87a604472b57a4425f7b90f6
Synergized <synergized@iMac-de-Synergized.local>
2017-06-07 14:37:44 +0200

    [CLEAN] - Folder cleanup

--- generators/formlist/templates/ts/list/list_model.ts
+++
@@ -1,7 +0,0 @@
-import { Stream } from 'xstream'
-
-
-export function model(actions):{ states:Stream<{}> } {
-  const states:Stream<{}> = actions.fold((state, action) => action(state), { requests: {} })
-  return { states }
-}
