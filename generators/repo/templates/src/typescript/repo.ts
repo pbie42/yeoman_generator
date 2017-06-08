3e623a5575289d8abc441d2d63a09ecb8eb7f7ec
Synergized <synergized@iMac-de-Synergized.local>
2017-06-08 14:41:57 +0200

    [Rework] - repo now convereted to typescript

--- generators/repo/templates/src/typescript/repo.ts
+++ generators/repo/templates/src/typescript/repo.ts
@@ -3,27 +3,81 @@ import { HTTPSource } from '@cycle/http'
 
 import { log, sample, bind, assign } from '../utils'
 
-export const Status = { pending: { status: "Pending" }, success: { status: "Success" }, failure: { status: "Failure" } }
+interface PostRequest {
+    method:string
+    headers:{ 'Content-Type':string }
+    url:string
+    category:string
+    send:Stream<any>
+  }
 
-function startQuery(category, state) {
-  return assign({}, state, { requests:  assign({}, state.requests, { [category]: Status.pending }) } )
+interface GetRequest {
+  method:string
+  url:string
+  category:string
 }
 
-function querySuccess(category, state) {
-  return assign({}, state, { requests:  assign({}, state.requests, { [category]: Status.success }) } )
+interface NowOn {
+  now:Function
+  on:Function
 }
 
-function queryFailure(category, state) {
-  return assign({}, state, { requests:  assign({}, state.requests, { [category]: Status.failure }) } )
+interface RequestAction {
+  request:Stream<Function>
+  action:Stream<Function>
 }
 
-function getStreamCtr(streamArg) {
+interface Status {
+  pending: {
+    status:string
+  }
+  success: {
+    status:string
+  } 
+  failure: {
+    status:string
+  }
+}
+
+interface State {
+  requests: {
+    [id: string]: {
+      status:string
+    }
+  }
+}
+
+type StreamArg = { request, action} | Function
+
+export const Status:Status = { pending: { status: "Pending" }, success: { status: "Success" }, failure: { status: "Failure" } }
+
+function startQuery(category:string, state:{ requests }):State {
+  return assign({}, state, { requests: assign({}, state.requests, { [category]: Status.pending }) } )
+}
+
+function querySuccess(category:string, state):State {
+  return assign({}, state, { requests: assign({}, state.requests, { [category]: Status.success }) } )
+}
+
+function queryFailure(category:string, state):State {
+  return assign({}, state, { requests: assign({}, state.requests, { [category]: Status.failure }) } )
+}
+
+function getStreamCtr(streamArg:StreamArg):Array<Function> {
   if (typeof streamArg == "function") { return [ streamArg, streamArg ] }
   return [ streamArg.request, streamArg.action ]
 }
 
-function genQuery(category, request, streamArg, HTTP) {
-  const response = HTTP.select(category).flatten().map(resp => JSON.parse(resp.text))
+interface GenQuery {
+  request:any
+  actions:any[]
+  response: {
+    [x:string]:Stream<HTTPSource>
+  }
+}
+
+function genQuery(category:string, request:PostRequest | GetRequest, streamArg:StreamArg, HTTP:HTTPSource):GenQuery {
+  const response:Stream<HTTPSource> = HTTP.select(category).flatten().map(resp => JSON.parse(resp.text))
   const [requestStreamCtr, actionStreamCtr] = getStreamCtr(streamArg)
   const query = {
     request: requestStreamCtr(request),
@@ -36,17 +90,17 @@ function genQuery(category, request, streamArg, HTTP) {
   return query
 }
 
-function getNow(url:string, category:string) {
-  const request = { method: 'GET', url, category }
+function getNow(url:string, category:string):Function {
+  const request:GetRequest = { method: 'GET', url, category }
   return bind(genQuery, category, request, Stream.of.bind(Stream))
 }
 
-function getOn(url:string, category:string, stream:Stream<any>) {
-  const request = { method: 'GET', url, category }
+function getOn(url:string, category:string, stream:Stream<any>):Function {
+  const request:GetRequest = { method: 'GET', url, category }
   return bind(genQuery, category, request, stream.mapTo.bind(stream))
 }
 
-function postRequest(url, category, data) {
+function postRequest(url, category, data):PostRequest {
   return {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -55,29 +109,29 @@ function postRequest(url, category, data) {
   }
 }
 
-function postNow(url:string, category:string, data:Stream<any>) {
-  const request = postRequest(url, category, data)
+function postNow(url:string, category:string, data:Stream<any>):Function {
+  const request:PostRequest = postRequest(url, category, data)
   return bind(genQuery, category, request, Stream.of.bind(Stream))
 }
 
-function postOn(url, category, stream) {
-  const request = bind(postRequest, url, category)
-  const streamCtrs = {
+function postOn(url:string, category:string, stream:Stream<any>):Function {
+  const request:PostRequest = bind(postRequest, url, category)
+  const streamCtrs:RequestAction = {
     request: stream.map.bind(stream),
     action: stream.mapTo.bind(stream)
   }
   return bind(genQuery, category, request, streamCtrs)
 }
 
-function queryBuilder(url:string, category:string, nowFn:Function, onFn:Function) {
+function queryBuilder(url:string, category:string, nowFn:Function, onFn:Function):NowOn {
   return {
-    now() { return nowFn(url, category) },
-    on(stream) { return onFn(url, category, stream) }
+    now():Function { return nowFn(url, category) },
+    on(stream):Function { return onFn(url, category, stream) }
   }
 }
 
 export const Repo = {
-  setup(...queryCtrs) {
+  setup(...queryCtrs):Function {
     return (HTTP:HTTPSource) => {
       const queries = queryCtrs.map(ctr => ctr(HTTP))
       return {
@@ -87,10 +141,10 @@ export const Repo = {
       }
     }
   },
-  get(url:string, category:string) {
+  get(url:string, category:string):NowOn {
     return queryBuilder(url, category, getNow, getOn)
   },
-  post(url:string, category:string) {
+  post(url:string, category:string):NowOn {
     return queryBuilder(url, category, postNow, postOn)
   }
 }
